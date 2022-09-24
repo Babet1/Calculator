ade26ba0b5a3bd3501f4b16061d9346ca0d80eea
Albert Radja Sihite <113775702+Babet1@users.noreply.github.com>
2022-09-24 20:10:28 +0700

    Calculator - Skilvul
    Tugas Calculator

---
+++ Kalkulator/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace Kalkulator
+{
+    public class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            float angka1;
+            float angka2 ;
+            int operasi;
+            float hasil ;
+            float Hasil;
+            
+
+
+           Console.WriteLine("Enter the action to be performed");
+           Console.WriteLine("---------------------------------");
+           Console.WriteLine("Press 1 for Addition");
+           Console.WriteLine("Press 2 for Substraction");
+           Console.WriteLine("Press 3 for Multiplication");
+           Console.WriteLine("Press 4 for Division");
+
+           operasi = int.Parse(Console.ReadLine());
+
+        
+            Console.Write("Enter 1st Input = ");
+            angka1 = float.Parse(Console.ReadLine());
+            Console.Write("Enter 2nd Input = ");
+            angka2 = float.Parse(Console.ReadLine());
+
+
+             if (operasi == 1)
+           {
+            hasil = angka1 + angka2;
+            
+           }
+            else if (operasi == 2)
+           {
+            hasil = angka1 - angka2;
+            Console.WriteLine($"The result is {hasil}");
+           }
+            else if (operasi == 3)
+           {
+            hasil = angka1 * angka2;
+            Console.WriteLine($"The result is {hasil}");
+           }
+            else if (operasi == 4)
+           {
+            hasil =  angka1 / angka2;
+            Console.WriteLine($"The result is {hasil}");
+           }
+            else 
+           {
+            Console.WriteLine("Operation eror!");
+           }
+           
+
+           
+            
+        }
+    }
+}
